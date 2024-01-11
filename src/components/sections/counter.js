f90ee62302b2d59214e7a3c3d8edc4248ab114fa
Resources-DTW <resource.dtw@gmail.com>
2024-01-11 18:38:37 +0530

    11/1 - commit

--- src/components/sections/counter.js
+++ src/components/sections/counter.js
@@ -1,23 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap";
 import dots from "../../assets/images/dots.png";
 
-const Counter = ({counterValues}) => {
+const Counter = ({ counterValues }) => {
   return (
     <Container>
-    <div className="counter-design d-md-flex align-items-center justify-content-between mt-4">
-      <img src={dots} alt="dots" className="dots-top" />
-       <img src={dots} alt="dots" className="dots-bottom" />
+      <div className="counter-design d-md-flex align-items-center justify-content-between mt-4">
+        <img src={dots} alt="dots" className="dots-top" />
+        <img src={dots} alt="dots" className="dots-bottom" />
 
-      <div className="w-100">
-        <Row className="counter-design-mob">
-          {counterValues.map((counter => (
+        <div className="w-100">
+          <Row className="counter-design-mob">
+            {counterValues.map((counter) => (
               <Col lg={3} md={6} xs={6} key={counter.id}>
-                <h2 className="counter-value">{counter.value}<span>+</span></h2>
+                <h2 className="counter-value">
+                  {counter.value}
+                  <span>+</span>
+                </h2>
                 <p className="counter-title">{counter.title}</p>
               </Col>
-          )))}
-          
-          {/* <Col lg={3} md={6}>
+            ))}
+
+            {/* <Col lg={3} md={6}>
             <h2 className="counter-value">50<span>+</span></h2>
             <p className="counter-title">Students Enrolled</p>
           </Col>
@@ -29,12 +32,11 @@ const Counter = ({counterValues}) => {
             <h2  className="counter-value">20<span>+</span></h2>
             <p className="counter-title">Workshops Conducted</p>
           </Col> */}
-        </Row>
+          </Row>
+        </div>
       </div>
-  
-    </div>
-  </Container>
-  )
-}
+    </Container>
+  );
+};
 
 export default Counter;
