41cf012652c5eec57d406499f9158f78a4c5954a
Resources-DTW <resource.dtw@gmail.com>
2024-01-19 18:59:16 +0530

    added auto scroll down in upcoming page

--- src/pages/upcoming-batches.js
+++ src/pages/upcoming-batches.js
@@ -10,8 +10,18 @@ import BookaSeat from "../components/sections/book-a-seat";
 import { Helmet } from "react-helmet";
 
 const UpcomingBatches = () => {
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, []);
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const middleDiv = document.getElementById("upcoming");
+    if (middleDiv) {
+      const middleDivOffset = middleDiv.offsetTop;
+      const windowHeight = window.innerHeight;
+      const scrollTo = middleDivOffset - windowHeight / 2;
+      window.scrollTo(0, scrollTo);
+    }
   }, []);
 
   const { show, handleClose, handleShow, bookMySeat, upComingBatchesCounter } =
@@ -65,7 +75,7 @@ const UpcomingBatches = () => {
       </section>
 
       <section className="section">
-        <div className="position-relative">
+        <div className="position-relative" id="upcoming">
           <div className="design-circle-about-us-counter position-absolute"></div>
           {/* UI/UX Design Course */}
           <UiUxCourse bookMySeat={bookMySeat} />
